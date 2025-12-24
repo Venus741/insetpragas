c9395e15200120f3ce9c4348b92625735c5f6785
Venus741 <veniciostenorio1@gmail.com>
2025-12-24 12:34:34 -0300

    Blue columns have been added between the infoboxes

--- src/components/main/pestControl.tsx
+++ src/components/main/pestControl.tsx
@@ -1,16 +1,29 @@
 export default function PestControl() {
     return(
         <div className="flex justify-center bg-white">
-            <section className="flex -mt-28 transparent w-auto h-60 justify-center text-center rounded-x1 mb-8 shadow-xl">
+            <section className="flex -mt-28 bg-white w-auto h-60 justify-center text-center rounded-xl mb-8 shadow-xl">
 
-                <article className="flex justify-center items-center bg-white w-90 rounded-l-xl">
-                    <p>Certificados válidos</p>
+                <article className="flex flex-col justify-center items-center bg-white w-90 rounded-l-xl px-3">
+                    <h1 className="font-bold">Certificados válidos</h1>
+                    <p>De acordo com as normas sanitárias e ambientais exigidas pelos órgãos competentes.</p>
                 </article>
-                <article className="flex justify-center items-center bg-white w-90 border-x-3 border-indigo-500">
-                    <p>Atendemos em toda Alagoas</p>
+
+                <article className="flex w-1 shadow-xl h-full justify-center items-center">
+                    <div className="bg-blue-300 w-full h-30 rounded-full inset-shadow-sm inset-shadow-white"></div>
                 </article>
-                <article className="flex justify-center items-center bg-white w-90 rounded-r-xl">
-                    <p>Produto sem cheiro</p>
+
+                <article className="flex flex-col justify-center items-center bg-white w-90 px-3">
+                    <h1 className="font-bold">Atendemos em toda Alagoas</h1>
+                    <p>Com cobertura completa, realizamos atendimentos, com soluções profissionais e seguras onde você estiver.</p>
+                </article>
+
+                <article className="flex w-1 shadow-xl h-full justify-center items-center">
+                    <div className="bg-blue-300 w-full h-30 rounded-full inset-shadow-sm inset-shadow-white"></div>
+                </article>
+
+                <article className="flex flex-col justify-center items-center bg-white w-90 rounded-r-xl px-3">
+                    <h1 className="font-bold">Produto sem cheiro</h1>
+                    <p>Utilizamos produtos sem cheiro, garantindo um ambiente seguro e livre de odores após a dedetização.</p>
                 </article>
             </section>
         </div>
