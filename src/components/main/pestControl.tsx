7e1fc676293684ee9c4805de444b570a4d478d1b
Venus741 <veniciostenorio1@gmail.com>
2025-12-24 00:39:55 -0300

    Styling Infobar

--- src/components/main/pestControl.tsx
+++ src/components/main/pestControl.tsx
@@ -1,11 +1,18 @@
 export default function PestControl() {
     return(
-        <div className="flex justify-center bg-red-200">
-            <div className="bg-blue-400">
-                <ul>Certificados válidos</ul>
-                <ul>Atendemos em toda Alagoas</ul>
-                <ul></ul>
-            </div>
+        <div className="flex justify-center bg-white">
+            <section className="flex -mt-28 transparent w-auto h-60 justify-center text-center rounded-x1 mb-8 shadow-xl">
+
+                <article className="flex justify-center items-center bg-white w-90 rounded-l-xl">
+                    <p>Certificados válidos</p>
+                </article>
+                <article className="flex justify-center items-center bg-white w-90 border-x-3 border-indigo-500">
+                    <p>Atendemos em toda Alagoas</p>
+                </article>
+                <article className="flex justify-center items-center bg-white w-90 rounded-r-xl">
+                    <p>Produto sem cheiro</p>
+                </article>
+            </section>
         </div>
     )
 }
