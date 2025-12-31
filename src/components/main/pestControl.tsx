74417e0fc3787804a71005a4be97f635652e0bc3
Venus741 <veniciostenorio1@gmail.com>
2025-12-30 21:58:17 -0300

    Adding images to service containers

--- src/components/main/pestControl.tsx
+++ src/components/main/pestControl.tsx
@@ -1,31 +1,18 @@
 export default function PestControl() {
     return(
-        <div className="flex justify-center bg-white">
-            <section className="flex -mt-28 bg-white w-auto h-60 justify-center text-center rounded-xl mb-8 shadow-xl">
-
-                <article className="flex flex-col justify-center items-center bg-white w-90 rounded-l-xl px-3">
-                    <h1 className="font-bold">Certificados válidos</h1>
-                    <p>De acordo com as normas sanitárias e ambientais exigidas pelos órgãos competentes.</p>
-                </article>
-
-                <article className="flex w-1 shadow-xl h-full justify-center items-center">
-                    <div className="bg-blue-300 w-full h-30 rounded-full inset-shadow-sm inset-shadow-white"></div>
-                </article>
-
-                <article className="flex flex-col justify-center items-center bg-white w-90 px-3">
-                    <h1 className="font-bold">Atendemos em toda Alagoas</h1>
-                    <p>Com cobertura completa, realizamos atendimentos, com soluções profissionais e seguras onde você estiver.</p>
-                </article>
-
-                <article className="flex w-1 shadow-xl h-full justify-center items-center">
-                    <div className="bg-blue-300 w-full h-30 rounded-full inset-shadow-sm inset-shadow-white"></div>
-                </article>
-
-                <article className="flex flex-col justify-center items-center bg-white w-90 rounded-r-xl px-3">
-                    <h1 className="font-bold">Produto sem cheiro</h1>
-                    <p>Utilizamos produtos sem cheiro, garantindo um ambiente seguro e livre de odores após a dedetização.</p>
-                </article>
-            </section>
-        </div>
+        <section className="flex w-full h-150 p-7">
+            <div className=" flex w-400">
+                <div className="bg-[url('/images/pest-control.png')] bg-contain bg-no-repeat bg-center w-full h-auto flex-1 flex items-center"></div>
+            </div>
+           
+            <div className="bg-blue-200 w-400">
+                <div className="flex w-full h-50 bg-blue-500 items-center p-3">
+                    <h2 className="flex font-bold text-3xl">Focados em práticas sustentáveis de controle de pragas</h2>
+                </div>
+                <div className="p-3">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut reprehenderit, accusantium quidem suscipit doloribus quo earum, voluptatum odio voluptatem, mollitia fugit ad enim nam minima error pariatur explicabo sunt vero?</p>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/components/main/waterTank.tsx
+++ src/components/main/waterTank.tsx
@@ -1,14 +1,18 @@
-export default function WaterTank() {
+export default function PestControl() {
     return(
-        <div className="flex wf-full h-full bg-pink-500 w-full h-200">
-            <p>tank</p>
-            <div>
-                <div className="bg-[url('/images/service2.png')] bg-cover bg-center w-full h-auto"></div>
+        <section className="flex w-full h-150 p-7">
+            <div className="flex w-400">
+                <div className="bg-[url('/images/water-tank-clean.png')] bg-contain bg-no-repeat bg-center w-full h-auto flex-1 flex items-center"></div>
             </div>
-
-            <div>
-                 
+           
+            <div className="bg-blue-200 w-400">
+                <div className="flex w-full h-50 bg-blue-500 items-center p-3">
+                    <h2 className="flex font-bold text-3xl">Limpeza de caixa d'águas e sisternas</h2>
+                </div>
+                <div className="p-3">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut reprehenderit, accusantium quidem suscipit doloribus quo earum, voluptatum odio voluptatem, mollitia fugit ad enim nam minima error pariatur explicabo sunt vero?</p>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
