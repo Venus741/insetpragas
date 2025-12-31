fbae5e3dd00af30fe38275fd6e68a2e6798faaba
Venus741 <veniciostenorio1@gmail.com>
2025-12-31 00:37:59 -0300

    Add images in service cards

--- src/components/main/pestControl.tsx
+++ src/components/main/pestControl.tsx
@@ -1,17 +1,26 @@
+import Contact from "../header/contactButton";
+
 export default function PestControl() {
     return(
         <section className="flex w-full h-150 p-7">
             <div className=" flex w-400">
                 <div className="bg-[url('/images/pest-control.png')] bg-contain bg-no-repeat bg-center w-full h-auto flex-1 flex items-center"></div>
+
             </div>
            
-            <div className="bg-blue-200 w-400">
-                <div className="flex w-full h-50 bg-blue-500 items-center p-3">
-                    <h2 className="flex font-bold text-3xl">Focados em práticas sustentáveis de controle de pragas</h2>
+            <div className="w-400">
+                <div className="flex w-full h-50 items-center p-3">
+                    <h2 className="flex font-bold font-sans text-3xl">Focados em práticas sustentáveis de controle de pragas</h2>
+
                 </div>
-                <div className="p-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut reprehenderit, accusantium quidem suscipit doloribus quo earum, voluptatum odio voluptatem, mollitia fugit ad enim nam minima error pariatur explicabo sunt vero?</p>
+
+                <div className="p-3 mb-20">
+                    <p>Somos focados em práticas sustentáveis de controle de pragas, utilizando métodos eficazes que reduzem o impacto ambiental e preservam a saúde das pessoas e dos animais. Atuamos com responsabilidade, tecnologia e respeito à natureza para garantir ambientes protegidos de forma segura e consciente.
+                    </p>
+
                 </div>
+                
+                <Contact/>
             </div>
         </section>
     )
--- src/components/main/waterTank.tsx
+++ src/components/main/waterTank.tsx
@@ -1,17 +1,26 @@
+import Contact from "../header/contactButton";
+
 export default function PestControl() {
     return(
         <section className="flex w-full h-150 p-7">
             <div className="flex w-400">
                 <div className="bg-[url('/images/water-tank-clean.png')] bg-contain bg-no-repeat bg-center w-full h-auto flex-1 flex items-center"></div>
+
             </div>
            
-            <div className="bg-blue-200 w-400">
-                <div className="flex w-full h-50 bg-blue-500 items-center p-3">
-                    <h2 className="flex font-bold text-3xl">Limpeza de caixa d'águas e sisternas</h2>
+            <div className="w-400">
+                <div className="flex w-full h-50 items-center p-3">
+                    <h2 className="flex font-bold text-3xl">Serviço especializado de limpeza de caixas d’água e cisternas</h2>
+
                 </div>
-                <div className="p-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut reprehenderit, accusantium quidem suscipit doloribus quo earum, voluptatum odio voluptatem, mollitia fugit ad enim nam minima error pariatur explicabo sunt vero?</p>
+
+                <div className="p-3 mb-20">
+                    <p>Somos especializados na limpeza de caixas d’água e cisternas, utilizando procedimentos seguros e eficientes que garantem a qualidade da água e a proteção da saúde. Atuamos com responsabilidade, cuidado e técnicas adequadas para manter seu abastecimento limpo, seguro e confiável.
+                    </p>
+
                 </div>
+
+                <Contact/>
             </div>
         </section>
     )
