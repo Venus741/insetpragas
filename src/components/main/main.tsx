688c8c33bcf9f2d450b961a69e98ff77b4cd04a4
Venus741 <veniciostenorio1@gmail.com>
2026-01-08 23:52:48 -0300

    chore: merge build-main into main

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,9 +5,12 @@ import WaterTank from "./waterTank";
 export default function Main() {
     return(
         <section className="w-full h-full">
+
+        <div className="w-full h-full">
             <CentralCard/>
             <PestControl/>
             <WaterTank/>
+        </div>
         </section>
     )
 }
