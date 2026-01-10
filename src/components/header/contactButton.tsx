df2ef6665c69c7793d59b63e3935cb1118945395
Venus741 <veniciostenorio1@gmail.com>
2026-01-09 22:45:44 -0300

    add link to whatsapp button

--- src/components/header/contactButton.tsx
+++ src/components/header/contactButton.tsx
@@ -1,7 +1,14 @@
 export default function Contact() {
     return(
         <div className="flex">
-            <button className="p-2 rounded-full text-white font-bold bg-green-500 text-shadow-sm">Contate-nos pelo whatsapp</button>
+            <a
+                href="https://wa.me/558291767257?text=Olá!%20Gostaria%20de%20um%20orçamento."
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-2 rounded-full text-white font-bold bg-green-500 text-shadow-sm hover:bg-green-600 transition"
+            >
+                Contate-nos pelo WhatsApp
+            </a>
         </div>
     )
 }
