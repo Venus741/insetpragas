f1230a32610c66e1da728946efee4b138a3b3d85
Venus741 <veniciostenorio1@gmail.com>
2026-01-15 21:21:40 -0300

    edit company name on footer

--- src/components/footer/logo.tsx
+++ src/components/footer/logo.tsx
@@ -4,7 +4,7 @@ export default function InsetpragasLogo() {
             <div className="bg-[url('/images/logo.png')] bg-contain bg-no-repeat bg-center w-full h-auto flex-1 flex items-center"></div>
 
             <div className="w-full justify-center flex">
-                <p className="font-bold text-white">INSETPRAGAS</p>
+                <p className="font-bold text-white">INSETPRAGAS MACEIÓ</p>
             </div>
         </section>
     )
