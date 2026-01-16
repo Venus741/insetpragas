01f50298d5d6eb4cfc66fe63f5f6e13628096185
Venus741 <veniciostenorio1@gmail.com>
2026-01-15 22:19:47 -0300

    change button name

--- src/components/header/contactButton.tsx
+++ src/components/header/contactButton.tsx
@@ -5,9 +5,9 @@ export default function Contact() {
                 href="https://wa.me/558291767257?text=Olá!%20Gostaria%20de%20um%20orçamento."
                 target="_blank"
                 rel="noopener noreferrer"
-                className="p-2 rounded-full text-white font-bold bg-green-500 text-shadow-sm hover:bg-green-600 transition"
+                className="p-2 px-4 rounded-full text-white font-bold bg-green-500 text-shadow-sm hover:bg-green-600 transition"
             >
-                Contate-nos pelo WhatsApp
+                Agende um orçamento
             </a>
         </div>
     )
